ce84024ffe1b93dd323908484b045766f2d3e39d
Neozuni <30896375+Neozuni@users.noreply.github.com>
2017-10-17 16:13:58 +0900

    update to Model2 board
    for test

--- self_Board/src/board/model/BoardDAO.java
+++ self_Board/src/board/model/BoardDAO.java
@@ -52,7 +52,7 @@ public class BoardDAO {
 			Connection conn = null;
 			PreparedStatement ps = null;
 			ResultSet rs = null;
-			List<BoardVO> arilcleList = null;
+			List<BoardVO> articleList = null;
 			
 			try {
 				conn = ConnUtil.getConnection();
@@ -132,7 +132,7 @@ public class BoardDAO {
 				ps.setString(2, article.getEmail());
 				ps.setString(3, article.getSubject());
 				ps.setString(4, article.getPass());
-				ps.setString(5, article.getRegdate());
+				ps.setTimestamp(5, article.getRegdate());
 				ps.setInt(6, ref);
 				ps.setInt(7, step);
 				ps.setInt(8, depth);
@@ -187,24 +187,119 @@ public class BoardDAO {
 			return article;
 		}
 		
+		
 		public BoardVO updateGetArticle(int num) {
-			Connention conn = null;
+			Connection conn = null;
 			PreparedStatement ps = null;
 			ResultSet rs = null;
 			BoardVO article = null;
-				try {
-					conn = ConnUtil.getConnection();
-					ps = conn.prepareStatement("select * from board where num =?");
-					ps.setInt(1, num);
-					rs = ps.executeQuery();
-					if(rs.next()) {
-						article = new BoardVO();
-						article.setNum(num);
-					}
-					
-				} catch (Exception ex) {
+			
+			try {
+				conn = ConnUtil.getConnection();
+				ps = conn.prepareStatement("select * from board where num = ?");
+				ps.setInt(1, num);
+				rs = ps.executeQuery();
+				
+				if (rs.next()) {
+					article = new BoardVO();
+					article.setNum(rs.getInt("num"));
+					article.setWriter(rs.getString("writer"));
+					article.setEmail(rs.getString("email"));
+					article.setSubject(rs.getString("subject"));
+					article.setPass(rs.getString("pass"));
+					article.setRegdate(rs.getTimestamp("regdate"));
+					article.setReadcount(rs.getInt("readcount"));
+					article.setRef(rs.getInt("ref"));
+					article.setStep(rs.getInt("step"));
+					article.setDepth(rs.getInt("depth"));
+					article.setContent(rs.getString("ip"));
+							
 				}
-		}
+				
+			} catch (Exception ex) {
+				ex.printStackTrace();
+			} finally {
+				if (rs!=null) try { rs.close(); }catch (SQLException ex) {}
+				if (ps!=null) try { ps.close(); }catch (SQLException ex) {}
+				if (conn!=null) try { conn.close(); }catch (SQLException ex) {}
+				}
+			return article;
+			}
 		
-}
 
+
+	public int updateArticle(BoardVO article) {
+		Connection conn = null;
+		PreparedStatement ps = null;
+		ResultSet rs = null;
+		String dbpasswd = "";
+		String sql = "";
+		int result = -1; // 결과값
+		
+		try {
+			conn = ConnUtil.getConnection();
+			ps = conn.prepareStatement("select pass from board where num = ?");
+			ps.setInt(1, article.getNum());
+			rs = ps.executeQuery();
+			
+			if (dbpasswd.equals(article.getPass())) {
+				sql = "update board set writer=?,email=?,subject=?";
+				sql += ",content=? where num=?";
+				ps = conn.prepareStatement(sql);
+				ps.setString(1, article.getWriter());
+				ps.setString(2, article.getEmail());
+				ps.setString(3, article.getSubject());
+				ps.setString(4, article.getContent());
+				ps.setInt(5, article.getNum());
+				ps.executeQuery();
+				result = 1;// 수정성공
+			} else {
+				result = 0;// 수정실패
+			}
+			
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		} finally {
+			if (rs!=null) try { rs.close(); }catch (SQLException ex) {}
+			if (ps!=null) try { ps.close(); }catch (SQLException ex) {}
+			if (conn!=null) try { conn.close(); }catch (SQLException ex) {}
+			}
+		return result;
+		}
+	
+
+
+	public int deleteArticle(int num, String pass) {
+		Connection conn = null;
+		PreparedStatement ps = null;
+		ResultSet rs = null;
+		String dbPass = "";
+		int result = -1;
+		
+		try {
+			conn = ConnUtil.getConnection();
+			ps = conn.prepareStatement("select pass from board where num = ?");
+			ps.setInt(1, num);
+			rs = ps.executeQuery();
+			
+			if (rs.next()) {
+				dbPass = rs.getString("pass");
+				if (dbPass.equals(pass)) {
+					ps = conn.prepareStatement("delete from board where num=?");
+					ps.setInt(1, num);
+					ps.executeQuery();
+					result = 1;//글 삭제 성공
+				} else 
+					result = 0;//비밀번호 틀
+				}
+			
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		} finally {
+			if (rs!=null) try { rs.close(); }catch (SQLException ex) {}
+			if (ps!=null) try { ps.close(); }catch (SQLException ex) {}
+			if (conn!=null) try { conn.close(); }catch (SQLException ex) {}
+			}
+		return result;
+		}
+	}
